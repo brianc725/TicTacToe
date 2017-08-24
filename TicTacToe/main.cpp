f754e8d3592402e036f2ec06058ba555149ab065
brianc725 <briandy381@aol.com>
2017-08-23 20:17:44 -0700

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Brian Chan on 8/23/17.
+//  Copyright © 2017 Brian Chan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
